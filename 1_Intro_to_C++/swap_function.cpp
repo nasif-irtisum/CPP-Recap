08db0003572df77aaac78b28adda8176b0b5c864
nirtisum-cs <nirtisum.cs@gmail.com>
2026-03-02 02:00:26 +0600

    Add swap function

---
+++ 1_Intro_to_C++/swap_function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int a, b; cin >> a >> b;
+
+    swap(a,b);
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
