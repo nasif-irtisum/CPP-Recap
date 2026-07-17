a60df768ced54585047c0fbf18a371b1e0e4efca
nirtisum-cs <nirtisum.cs@gmail.com>
2026-07-18 03:11:51 +0600

    Element Access in String

---
+++ 4_String_Class/ElementAccess.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    string s = "Hello";
+    cout << s.at(0) << endl;
+    cout << s.back() << endl;
+    cout << s.front () << endl;
+}
